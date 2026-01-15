78cf79b020daefb6cc9d7d4d352639feec32aa69
shubham <vermashubham4925@gmail.com>
2026-01-15 17:12:59 +0530

    updatedmanyt

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ const humidity = document.querySelector(".details .card h3");
 const WindSpeed = document.querySelector(".wind");
 const Sunrise = document.querySelector(".risetime");
 const Sunset = document.querySelector(".settime");
-const weatherapp = document.querySelector(".weather-app video");
+const weatherapp = document.querySelector(".backvideo");
 const Pressure = document.querySelector(".pressure");
 const Visibility = document.querySelector(".visibility");
 
+// Day Loading Bar
+const sunSection = document.querySelector(".sun-section")
+const sunLine = document.querySelector(".sun-line");
+const sunIndicator = document.querySelector(".sun-indicator");
+const dayRise = document.querySelector(".day-rise")
+const daySet = document.querySelector(".day-set")
 
+// Night Loading Bar
+const nightSection = document.querySelector(".night-container")
+const nightLine = document.querySelector(".night-line");
+const nightIndicator = document.querySelector(".night-indicator");
+const nightRise = document.querySelector(".night-rise")
+const nightSet = document.querySelector(".night-set")
+
+const errorBox = document.getElementById("errorBox");
 
 function convertTime(unix) {
     let date = new Date(unix * 1000);
@@ -25,25 +39,26 @@ function convertTime(unix) {
     return `${h}:${m} ${ampm}`;
 }
 
+function showError(massage) {
+  errorBox.querySelector(".error-text").textContent = massage;
+  errorBox.classList.add("show");
 
-
-
-
-
+  setTimeout(() => {
+    errorBox.classList.remove("show");
+  }, 3000);
+}
 
 async function weather(city){
+
 try{
     let rawdata  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=83c6c5f9c9ca033e443730d50b128625`);
  if(!rawdata.ok){
-   
-      throw new Error("Somthing went wrong");
-      
-    }else{
+    showError("City Not Found!!")
+      throw new Error("Somthing went wrong");  
+    }else{ 
 
-    
 let realdata = await rawdata.json();
-console.log(realdata)
-    
+
 function imageRendering(){
     if (realdata.main.temp <= 0){
     Weatherimg.src = "./images/snow.png"
@@ -56,24 +71,38 @@ function imageRendering(){
 }
 }
 
+
 function daynight(){
     const sunrise = realdata.sys.sunrise * 1000;
 const sunset = realdata.sys.sunset * 1000;
 
 let currenttime = Date.now();
 
-if(currenttime >= sunrise && currenttime < sunset){
-    weatherapp.src = "./images/daycompress.mp4"
+
+const eveningStart = sunset - (2 * 60 * 60 * 1000);
+
+
+if(currenttime >= sunrise && currenttime < eveningStart){
+    weatherapp.src = "./images/clearweather.mp4"
+}else if(currenttime >= eveningStart && currenttime < sunset){
+ weatherapp.src = "./images/sunsetvideo.mp4"
 }else{
     weatherapp.src = "./images/nightsky2.mp4"
 }
 }
 
+function getCityTime(realdata) {
+  const localOffset = new Date().getTimezoneOffset() * 60000;
+  return Date.now() + localOffset + realdata.timezone * 1000;
+}
 
-daynight()
+function toCityTime(unix, timezone) {
+  const localOffset = new Date().getTimezoneOffset() * 60000;
+  return unix * 1000 + localOffset + timezone * 1000;
+}
 
-imageRendering()
 
+function submitDetail(){
 Temperature.textContent = Math.floor(realdata.main.temp) + `°C`;
 City.textContent = realdata?.name
   ? realdata.name + (realdata?.sys?.country ? " " + realdata.sys.country : "")
@@ -90,17 +119,107 @@ Visibility.textContent = km +" "+"km"
 Sunrise.textContent = convertTime(realdata.sys.sunrise);
 
 Sunset.textContent = convertTime(realdata.sys.sunset);
+}
+
+function dayLoading(realdata, curr, sunrise, sunset){
+dayRise.textContent = convertTime(realdata.sys.sunrise )
+daySet.textContent = convertTime(realdata.sys.sunset)
 
 
-
+if(curr <= sunrise){
+ sunLine.style.width = "0%" 
+ sunIndicator.style.left = '0%'
+}else if(curr >= sunset){
+sunLine.style.width = "100%" 
+sunIndicator.style.left = '100%'
+}
+else{
+    let total = sunset - sunrise;
+    let passed = curr - sunrise
+    let per = (passed / total) * 100;
+    
+    let p = Math.floor(per)
+    console.log(p)
+     sunLine.style.width = p + '%' 
+     sunIndicator.style.left = p + '%'
 }
 }
+
+function nightLoading(realdata, curr, sunrise, sunset) {
+  nightSet.textContent = convertTime(realdata.sys.sunrise )
+nightRise.textContent = convertTime(realdata.sys.sunset)
+
+ 
+  console.log(curr)
+
+
+  const isNight = curr < sunrise || curr > sunset;
+
+
+  if (!isNight) {
+   
+    nightLine.style.width = "0%";
+    nightIndicator.style.left = "0%";
+    return;
+  }
+
+  
+ if (sunset < sunrise) {
+  sunset += 24 * 60 * 60 * 1000;
+}
+
+let nightStart = sunset;
+let nightEnd   = sunrise + 24 * 60 * 60 * 1000;
+
+if (curr < sunrise) {
+  curr += 24 * 60 * 60 * 1000;
+}
+
+let percent = ((curr - nightStart) / (nightEnd - nightStart)) * 100;
+percent = Math.min(Math.max(percent, 0), 100);
+
+nightLine.style.width = percent + "%";
+nightIndicator.style.left = percent + "%";
+;
+}
+
+function toggleDayNight(realdata) {
+
+  const curr = getCityTime(realdata);
+
+  const sunrise = toCityTime(realdata.sys.sunrise, realdata.timezone);
+  const sunset  = toCityTime(realdata.sys.sunset, realdata.timezone);
+
+  const isNight = curr > sunset || curr < sunrise;
+
+  sunSection.classList.add("hidden");
+  nightSection.classList.add("hidden");
+
+  if (isNight) {
+    nightSection.classList.remove("hidden");
+    nightLoading(realdata, curr, sunrise, sunset);
+  } else {
+    sunSection.classList.remove("hidden");
+    dayLoading(realdata, curr, sunrise, sunset);
+  }
+}
+
+
+imageRendering()
+daynight()
+toggleDayNight(realdata)
+submitDetail()
+
+    }
+    
+}
+
 catch(err){
     console.log(err.message)
 }
 
-
 }
+
 document.addEventListener("DOMContentLoaded",function(){
     input.focus();
     let savecity = localStorage.getItem("city");
@@ -109,16 +228,9 @@ document.addEventListener("DOMContentLoaded",function(){
     }
 })
 
-function showalert(){
-alertbox.style.display = "block";
- setTimeout(function(){
-     alertbox.style.display = "none";
- },3000)
-}
-
 Searchbtn.addEventListener("click",function(){
 if(input.value === "" ){
-  showalert()
+  showError("Enter City!!")
 }else{
         localStorage.setItem("city",input.value)
 weather(input.value)
